1e03d9b82037b7a42468c0f54c3774a165e31397
Shubhanshu Arya <aryashubhanshu20@gmail.com>
2024-03-16 00:54:12 +0530

    Hero Section Structure

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,7 +9,15 @@ import {
   yellowImg,
 } from "../utils";
 
-export const navLists = ["Store", "Mac", "iPhone", "Support"];
+export const navLists = [
+  "Store",
+  "Mac",
+  "iPhone",
+  "Watch",
+  "Airpods",
+  "TV & Home",
+  "Support",
+];
 
 export const hightlightsSlides = [
   {
